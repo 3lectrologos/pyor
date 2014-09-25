3f7fd68ad82dcf841d9a4ea653d62d3823de1d91
Alkis Gotovos <alkisg@inf.ethz.ch>
2014-09-25 17:04:25 +0200

    Add example provided by Lionel

--- demo.py
+++ demo.py
@@ -4,6 +4,6 @@ import pyor
 
 fin = 'data.txt'
 
-path = pyor.get_path(fin, 0, 142, edge_budget=20.0, time_limit=3.0,
+path = pyor.get_path(fin, 0, 1, edge_budget=3.780, time_limit=3.0,
                      plot=False, verbose=False)
 print 'path =', path
